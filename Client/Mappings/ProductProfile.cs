de5fa1fc973dcefa78184626a28445c179c9d31f
ThaiVanTinh <tinhthai15122003@gmail.com>
2024-11-07 00:23:29 +0700

    Home page

---
+++ Client/Mappings/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LaptopStore.Application.Features.Products.Commands.AddEdit;
+using LaptopStore.Domain.Entities.Catalog;
+
+namespace LaptopStore.Application.Mappings
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<AddEditProductCommand, Product>().ReverseMap();
+        }
+    }
+}
